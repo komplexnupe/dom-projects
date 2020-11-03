400d11e1548c3e62977bd0a803cc5414799de430
Komplexnupe <leonard.love87@icloud.com>
2020-11-03 12:51:19 -0500

    es6 conversion

---
+++ BookLists/appes6.js
@@ -0,0 +1,99 @@
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+class UI {
+  addBookToList(book) {
+    const list = document.getElementById('book-list');
+    // Create TR element
+    const row = document.createElement('tr');
+    // Insert columns
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+    `;
+    list.appendChild(row)
+  }
+
+  showAlert(message, className) {
+    // Create a <div>
+    const div = document.createElement('div');
+    // Add class
+    div.className = `alert ${className}`
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get a parent
+    const container = document.querySelector('.container');
+    // Get Form
+    const form = document.querySelector('#book-form');
+    // Insert before
+    container.insertBefore(div, form);
+    // Timeout
+    setTimeout(function () {
+      document.querySelector('.alert').remove();
+    }, 3000);
+  }
+
+  deleteBook(target) {
+    if(target.className === 'delete'){
+      target.parentElement.parentElement.remove();
+  }
+  }
+
+  clearFields() {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+  }
+}
+
+// Event Listeners for Add
+document.getElementById('book-form').addEventListener('submit',
+  function (e) {
+    e.preventDefault();
+    // console.log('test');
+    const title = document.getElementById('title').value,
+      author = document.getElementById('author').value,
+      isbn = document.getElementById('isbn').value
+
+    // Instantiate Book
+    const book = new Book(title, author, isbn);
+
+    // Instantiate UI
+    const ui = new UI();
+
+    // Validate
+    if (title === '' || author === '' || isbn === '') {
+      // Error alert
+      ui.showAlert('Please fill in all fields', 'error');
+    } else {
+      // Add book to list
+      ui.addBookToList(book);
+
+      // Show success
+      ui.showAlert('Book Added!', 'success');
+
+      // Clear fields
+      ui.clearFields();
+    }
+  });
+
+  // Event listeners for delete
+  document.getElementById('book-list').addEventListener('click', function(e){
+    e.preventDefault();
+    
+    // Instantiate UI
+    const ui = new UI();
+
+    // Delete target
+    ui.deleteBook(e.target);
+
+     // Show success
+     ui.showAlert('Book Removed!', 'success');
+  })
--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # dom-projects
-DOM Manipulation Practice
+Vanilla JS DOM Manipulation Practice
 
 ### Task List
 Simple app that adds and deletes tasks to and from Local Storage
 
 ### Loan Calculator
 Simple app calculates loan monthly payments and total interest over the life of the loan
+
+### Book List
+Simple app that adds and deletes books
