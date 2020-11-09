41d1245d7e7ac9e0fb4eb9538a6bd916c28cc52b
Komplexnupe <leonard.love87@icloud.com>
2020-11-09 15:05:47 -0500

    paint UI

--- WeatherJS/app.js
+++ WeatherJS/app.js
@@ -1,8 +1,17 @@
 // Instantiate weather object
 const weather = new Weather('Charlotte','NC');
+// Instantia
+const ui = new UI();
+// Get weather on DOM load
+document.addEventListener('DOMContentLoaded', getWeather);
 
+// weather.changeLocation('Miami', 'FL')
+
+function getWeather(){
 weather.getWeather()
   .then(data => {
-    console.log(data);
+    // console.log(data.name);
+    ui.paintUI(data)
   })
   .catch(err => console.log(err));
+}
--- WeatherJS/ui.js
+++ WeatherJS/ui.js
@@ -1,5 +1,26 @@
 class UI{
   constructor() {
-    // this.
+    this.location = document.getElementById('w-location');
+    this.desc = document.getElementById('w-desc');
+    this.tempString = document.getElementById('w-string');
+    this.details = document.getElementById('w-details');
+    this.icon = document.getElementById('w-icon');
+    this.humidity = document.getElementById('w-humidity');
+    this.wind = document.getElementById('w-wind');
+    this.feels = document.getElementById('w-feelsLike');
+  }
+
+  paintUI(weather) {
+    console.log(weather);
+    this.location.textContent = weather.name;
+    this.desc.textContent = weather.weather[0].description;
+    let tempF = (weather.main.temp - 273.15) * 1.80 + 32;
+    this.tempString.textContent = tempF.toFixed(0) + "℉";
+    let iconURL = "http://openweathermap.org/img/wn/" + weather.weather[0].icon + "@2x.png"
+    this.icon.setAttribute('src', iconURL);
+    this.humidity.textContent = `Relative Humidity: ${weather.main.humidity}`;
+    let tempFeels = (weather.main.feels_like - 273.15) * 1.80 + 32;
+    this.feels.textContent = `Feels Like: ${tempFeels.toFixed(0)}℉`;
+    this.wind.textContent = `Wind: ${weather.wind.speed}mph`
   }
 }
